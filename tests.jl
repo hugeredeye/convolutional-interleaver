104db5299c95093d86b734de910b1a8f79df4949
Катя <149680814+hugeredeye@users.noreply.github.com>
2024-12-24 20:35:55 +0300

    Update tests.jl

--- tests.jl
+++ tests.jl
@@ -1,18 +1,20 @@
-using CSV
-using DataFrames
-using Plots
-
 using Test
-include("interleave.jl")
+include("interleave.jl")  # Подключаем файл с функцией и структурой
 
-# Тесты для функции сверточного перемежения
-@testset "Interleaver Tests" begin
+# Подготовка тестов для функции interleave
+@testset "Interleave Tests" begin
+    # Тестовые данные
     testData = collect(2:25)
     regs = 3
     step = 3
     init = 2
+
+    # Создаем параметры
     params = InterleaveParams(testData, regs, step, init)
 
+    # Ожидаемый результат
     expected_output = [2, 2, 2, 5, 2, 2, 8, 2, 2, 11, 3, 2, 14, 6, 2, 17, 9, 2, 20, 12, 4, 23, 15, 7]
+
+    # Проверяем правильность работы функции
     @test interleave(params) == expected_output
 end
